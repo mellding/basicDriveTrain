fe4d1a16aa5e429cdbbdffaeb6dcc9e0c033de11
mellding <45488882+mellding@users.noreply.github.com>
2021-06-30 11:30:31 -0500

    added arcade drive

--- src/main/java/frc/robot/Contrants.java
+++ src/main/java/frc/robot/Contrants.java
@@ -1,5 +1,10 @@
 package frc.robot;
 
 public class Contrants {
+
+    //This sets the mas amperage of the drive train motors
+    public static final int driveMaxAmps = 20;
+
+
     
 }
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,12 +1,13 @@
 package frc.robot;
 
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.GenericHID;
 
 public class Robot extends TimedRobot {
   private static final String kDefaultAuto = "Default";
@@ -18,10 +19,12 @@ public class Robot extends TimedRobot {
   XboxController control00 = new XboxController(1);
 
   //Create Talon SRX motor Controllers
-  TalonSRX talon00 = new TalonSRX(0);
-  TalonSRX talon01 = new TalonSRX(1);
-  TalonSRX talon02 = new TalonSRX(2);
-  TalonSRX talon03 = new TalonSRX(3);
+  WPI_TalonSRX talonRight = new WPI_TalonSRX(0);
+  WPI_TalonSRX talonRight_follower = new WPI_TalonSRX(1);
+  WPI_TalonSRX talonLeft = new WPI_TalonSRX(2);
+  WPI_TalonSRX talonLeft_follower = new WPI_TalonSRX(3);
+
+  DifferentialDrive m_robotDrive = new DifferentialDrive(talonLeft, talonRight);
 
   /**
    * This function is run when the robot is first started up and should be used for any initialization code.
@@ -31,28 +34,23 @@ public class Robot extends TimedRobot {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
+
+    //Reset all the talons to defaul settings
+    talonRight.configFactoryDefault();
+    talonRight_follower.configFactoryDefault();
+    talonLeft.configFactoryDefault();
+    talonLeft_follower.configFactoryDefault();
+  
+    //Configure The two following controllers
+    talonRight_follower.follow(talonRight);
+    talonLeft_follower.follow(talonLeft);
+
+    
   }
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use this for items like
-   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
   @Override
   public void robotPeriodic() {}
 
-  /**
-   * This autonomous (along with the chooser code above) shows how to select between different
-   * autonomous modes using the dashboard. The sendable chooser code works with the Java
-   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
-   * uncomment the getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
-   * below with additional strings. If using the SendableChooser make sure to add them to the
-   * chooser code above as well.
-   */
   @Override
   public void autonomousInit() {
     m_autoSelected = m_chooser.getSelected();
@@ -80,7 +78,9 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    m_robotDrive.arcadeDrive(control00.getY(GenericHID.Hand.kLeft), control00.getX(GenericHID.Hand.kRight));
+  }
 
   /** This function is called once when the robot is disabled. */
   @Override
